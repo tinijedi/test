7d0cfbe0d780be54ff7c354f6f14c9332f59cfa7
Boyd Haynes <tinijedi@boyds-mac-mini.local>
2016-07-20 02:54:00 -0500

    after setting up view

---
+++ pokeGo/PokeCell.swift
@@ -0,0 +1,23 @@
+//
+//  PokeCell.swift
+//  pokeGo
+//
+//  Created by Boyd Haynes on 7/20/16.
+//  Copyright © 2016 Botany Bay Enterprises, Inc. All rights reserved.
+//
+
+import UIKit
+
+class PokeCell: UICollectionViewCell {
+    @IBOutlet weak var thumbImg: UIImageView!
+    @IBOutlet weak var nameLbl: UILabel!
+    
+    var pokemon: Pokemon!
+    
+    func configureCell(pokemon: Pokemon){
+        self.pokemon = pokemon
+        
+        nameLbl.text = self.pokemon.name.capitalizedString
+        thumbImg.image = UIImage(named: "\(self.pokemon.pokedexId)")
+    }
+}
